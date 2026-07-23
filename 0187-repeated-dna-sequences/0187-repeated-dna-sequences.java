c2c8a0e6fa222f552d5e34c000ca29e481a836f1
Rishabh Jaiswal <rishabhjaiswal16092004@gmail.com>
2026-07-23 11:01:23 +0530

    Time: 16 ms (98.54%), Space: 55.3 MB (91.13%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> repeat = new HashSet<>();
+
+        for(int i=0;i+9<s.length();i++){
+            String temp = s.substring(i,i+10);
+            if(!seen.add(temp)){
+                repeat.add(temp);
+            }
+        }
+
+        return new ArrayList<>(repeat);
+    }
+}
